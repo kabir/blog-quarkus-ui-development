fd163de3ac19fd4b9a236deb3059fdb1dcf6a36b
Kabir Khan <kkhan@redhat.com>
2019-05-14 22:32:53 +0100

    Step 4 - Filter to redirect to the angular app
    This only happens if it is not a file, rest or servlet call

---
+++ src/main/java/org/kabir/quarkus/ui/AngularRouteFilter.java
@@ -0,0 +1,39 @@
+package org.kabir.quarkus.ui;
+
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
+ */
+@WebFilter(urlPatterns = "/*")
+public class AngularRouteFilter extends HttpFilter {
+
+    private static final Pattern FILE_NAME_PATTERN = Pattern.compile(".*[.][a-zA-Z\\d]+");
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        chain.doFilter(request, response);
+
+        if (response.getStatus() == 404) {
+            String path = request.getRequestURI().substring(
+                    request.getContextPath().length()).replaceAll("[/]+$", "");
+            if (!FILE_NAME_PATTERN.matcher(path).matches()) {
+                // We could not find the resource, i.e. it is not anything known to the server (i.e. it is not a REST
+                // endpoint or a servlet), and does not look like a file so try handling it in the front-end routes.
+                request.getRequestDispatcher("/").forward(request, response);
+            }
+        }
+    }
+}
