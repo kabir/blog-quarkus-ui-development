84d25c5e96031887fa6645e3ac5249034f5747b8
Kabir Khan <kkhan@redhat.com>
2019-05-14 17:25:50 +0100

    Step 1 - Initial setup

---
+++ src/test/java/org/kabir/quarkus/ui/NativeSampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.kabir.quarkus.ui;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeSampleResourceIT extends SampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/org/kabir/quarkus/ui/SampleResourceTest.java
@@ -0,0 +1,21 @@
+package org.kabir.quarkus.ui;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class SampleResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
